59c0a724711b8001d41a2410fa11d5d30feac0e6
Radi Totev <raditotev@gmail.com>
2022-06-25 14:24:50 +0300

    Add schema and queries

---
+++ server/schema/schema.js
@@ -0,0 +1,65 @@
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+} from 'graphql';
+
+import { clients, projects } from '../sample-data.js';
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  },
+});
+
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve() {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(_, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve() {
+        return clients;
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(_, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+  },
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery,
+});
