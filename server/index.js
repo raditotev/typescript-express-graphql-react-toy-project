0c1af2c08944d66d1b074eb41fa6091cb18f21af
Radi Totev <raditotev@gmail.com>
2022-06-25 14:25:03 +0300

    Create GraphQL endpoint

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+
+import { schema } from './schema/schema.js';
 
 const app = express();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 const PORT = process.env.PORT || 8000;
 
