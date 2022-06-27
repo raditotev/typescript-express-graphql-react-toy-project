f4607ca127dde88644ef35ff04ec6aacbbec84f5
Radi Totev <raditotev@gmail.com>
2022-06-27 11:53:41 +0300

    Create Header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
