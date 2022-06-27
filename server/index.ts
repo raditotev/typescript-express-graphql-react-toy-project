1e49c9f7f69a25e4ef75eca1941f73d0cf109f11
Radi Totev <raditotev@gmail.com>
2022-06-27 13:02:48 +0300

    Add cors middleware

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import cors from 'cors';
 
 import { schema } from './schema/schema';
 import { connectDB } from './config/db';
 
 const app = express();
+app.use(cors());
 app.use(
   '/graphql',
   graphqlHTTP({
